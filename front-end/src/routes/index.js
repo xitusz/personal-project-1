c43e3919e7406b0c0f85576b491ffb0619d6bfbe
G-Alves1 <2kgabrielalves@gmail.com>
2023-04-19 16:30:08 -0300

    feat(routes): Login, Profile, Character, Region, Profile

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Character from "../pages/Character";
+import Region from "../pages/Region";
+import Profile from "../pages/Profile";
 
 const Router = () => (
   <Routes>
     <Route exact path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/character" element={<Character />} />
+    <Route path="/region" element={<Region />} />
+    <Route path="/profile" element={<Profile />} />
   </Routes>
 );
 
