206c1513651d06a2e4f04916de5d086c1e3f6afc
G-Alves1 <2kgabrielalves@gmail.com>
2023-04-20 01:01:43 -0300

    feat(pages): import component Header in Login

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../components/Button";
+import Header from "../components/Header";
 import {
   setItemToLocalStorage,
   getItemFromLocalStorage,
@@ -43,54 +44,60 @@ const Login = () => {
   };
 
   return (
-    <form className="container mt-4">
-      <h1 className="mb-3 text-center">Login</h1>
-      <div className="row align-items-center text-center">
-        <div className="col-md-10 mx-auto col-lg-5">
-          <div className="p-4 p-md-5 border rounded-3 bg-light mb-1">
-            <div className="input-group mb-3">
-              <span className="input-group-text">Email</span>
-              <div className="form-floating">
-                <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  name="email"
-                  placeholder="email@example.com"
-                  value={email}
-                  onChange={handleEmailChange}
-                  required
-                />
-                <label htmlFor="email">email@example.com</label>
+    <div>
+      <Header />
+      <form className="container mt-4">
+        <h1 className="mb-3 text-center">Login</h1>
+        <div className="row align-items-center text-center">
+          <div className="col-md-10 mx-auto col-lg-5">
+            <div className="p-4 p-md-5 border rounded-3 bg-light mb-1">
+              <div className="input-group mb-3">
+                <span className="input-group-text">Email</span>
+                <div className="form-floating">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    placeholder="email@example.com"
+                    value={email}
+                    onChange={handleEmailChange}
+                    required
+                  />
+                  <label htmlFor="email">email@example.com</label>
+                </div>
+              </div>
+              <div className="input-group">
+                <span className="input-group-text">Senha</span>
+                <div className="form-floating">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    name="password"
+                    placeholder="*********"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    required
+                  />
+                  <label htmlFor="password">*********</label>
+                </div>
               </div>
             </div>
-            <div className="input-group">
-              <span className="input-group-text">Senha</span>
-              <div className="form-floating">
-                <input
-                  type="password"
-                  className="form-control"
-                  id="password"
-                  name="password"
-                  placeholder="*********"
-                  value={password}
-                  onChange={handlePasswordChange}
-                  required
-                />
-                <label htmlFor="password">*********</label>
-              </div>
-            </div>
+            <Button
+              className="btn btn-primary w-100 mb-2"
+              onClick={handleLogin}
+            >
+              Entrar
+            </Button>
+            <Link to="/register">
+              <Button className="btn btn-secondary w-100">Registrar-se</Button>
+            </Link>
+            {error && <div className="mt-3 alert alert-danger">{error}</div>}
           </div>
-          <Button className="btn btn-primary w-100 mb-2" onClick={handleLogin}>
-            Entrar
-          </Button>
-          <Link to="/register">
-            <Button className="btn btn-secondary w-100">Registrar-se</Button>
-          </Link>
-          {error && <div className="mt-3 alert alert-danger">{error}</div>}
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
