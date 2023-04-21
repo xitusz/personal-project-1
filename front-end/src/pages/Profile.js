3fa00eda9ae899b11295eb1dc0fd363a1f763056
G-Alves1 <2kgabrielalves@gmail.com>
2023-04-21 05:21:24 -0300

    feat(pages): import component Header in Profile

---
+++ front-end/src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Profile = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Perfil</h1>
+    </div>
+  );
+};
+
+export default Profile;
