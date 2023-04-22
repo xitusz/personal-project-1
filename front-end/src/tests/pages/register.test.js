76739232c263378fbffade264209390fa3aaa462
G-Alves1 <2kgabrielalves@gmail.com>
2023-04-21 21:14:10 -0300

    test(pages): changes beforeEach, removes import from localStorage and resolve some tests

--- front-end/src/tests/pages/register.test.js
+++ front-end/src/tests/pages/register.test.js
@@ -3,46 +3,42 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { render, fireEvent, screen } from "@testing-library/react";
 import Register from "../../pages/Register";
-import { setItemToLocalStorage } from "../../services/localStorage";
-
-jest.mock("../../services/localStorage");
 
 describe("Register page", () => {
   beforeEach(() => {
+    localStorage.clear();
+  });
+
+  it("should renders the form correctly", () => {
     render(
       <BrowserRouter>
         <Register />
       </BrowserRouter>
     );
-  });
 
-  afterEach(() => {
-    window.localStorage.clear();
-  });
-
-  it("should renders the form correctly", () => {
-    const heading = screen.getByRole("heading", { name: /cadastre-se/i });
-    expect(heading).toBeInTheDocument();
-
-    const nameInput = screen.getByLabelText(/nome/i);
-    expect(nameInput).toBeInTheDocument();
-
-    const emailInput = screen.getByLabelText(/email@example.com/i);
-    expect(emailInput).toBeInTheDocument();
-
-    const passwordInput = screen.getByLabelText("*********");
-    expect(passwordInput).toBeInTheDocument();
-
-    const registerButton = screen.getByRole("button", { name: /cadastrar/i });
-    expect(registerButton).toBeInTheDocument();
-
-    const loginButton = screen.getByRole("button", {
-      name: /entrar/i,
-    });
-    expect(loginButton).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: /cadastre-se/i })
+    ).toBeInTheDocument();
+    expect(screen.getByLabelText(/nome/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/email@example.com/i)).toBeInTheDocument();
+    expect(screen.getByLabelText("*********")).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /cadastrar/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", {
+        name: /entrar/i,
+      })
+    ).toBeInTheDocument();
   });
 
   it("should updates the name,email and password fields correctly", () => {
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
+
     const nameInput = screen.getByLabelText(/nome/i);
     const emailInput = screen.getByLabelText(/email@example.com/i);
     const passwordInput = screen.getByLabelText("*********");
@@ -56,12 +52,18 @@ describe("Register page", () => {
     expect(passwordInput.value).toBe("password");
   });
 
-  it("should calls setItemToLocalStorage function with user name,email and password when Register button is clicked and redirect to login page", async () => {
+  it("should set item in local storage with user name,email and password when Register button is clicked and redirect to login page", async () => {
     const user = {
       name: "name",
       email: "email@example.com",
       password: "password",
     };
+
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
 
     const nameInput = screen.getByLabelText(/nome/i);
     const emailInput = screen.getByLabelText(/email@example.com/i);
@@ -77,11 +79,17 @@ describe("Register page", () => {
     fireEvent.change(passwordInput, { target: { value: user.password } });
     fireEvent.click(registerButton);
 
-    expect(setItemToLocalStorage).toHaveBeenCalledWith("userData", [user]);
+    expect(localStorage.getItem("userData")).toBe(JSON.stringify([user]));
     expect(window.location.pathname).toBe("/login");
   });
 
   it("should throw an error when name is invalid", () => {
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
+
     const nameInput = screen.getByLabelText(/nome/i);
     const emailInput = screen.getByLabelText(/email@example.com/i);
     const passwordInput = screen.getByLabelText("*********");
@@ -98,6 +106,12 @@ describe("Register page", () => {
   });
 
   it("should throw an error when email is invalid", () => {
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
+
     const nameInput = screen.getByLabelText(/nome/i);
     const emailInput = screen.getByLabelText(/email@example.com/i);
     const passwordInput = screen.getByLabelText("*********");
@@ -112,6 +126,12 @@ describe("Register page", () => {
   });
 
   it("should throw an error when password is invalid", () => {
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
+
     const nameInput = screen.getByLabelText(/nome/i);
     const emailInput = screen.getByLabelText(/email@example.com/i);
     const passwordInput = screen.getByLabelText("*********");
@@ -127,11 +147,56 @@ describe("Register page", () => {
     ).toBeInTheDocument();
   });
 
-  it("should throw an error when email is already registered", () => {});
+  it("should throw an error when email is already registered", () => {
+    const user = {
+      name: "name",
+      email: "email@example.com",
+      password: "password",
+    };
 
-  it("should redirect to home page if user is already logged in", () => {});
+    localStorage.setItem("userData", JSON.stringify([user]));
+
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
+
+    const nameInput = screen.getByLabelText(/nome/i);
+    const emailInput = screen.getByLabelText(/email@example.com/i);
+    const passwordInput = screen.getByLabelText("*********");
+    const registerButton = screen.getByRole("button", { name: /cadastrar/i });
+
+    fireEvent.change(nameInput, { target: { value: user.name } });
+    fireEvent.change(emailInput, {
+      target: { value: user.email },
+    });
+    fireEvent.change(passwordInput, { target: { value: user.password } });
+    fireEvent.click(registerButton);
+
+    expect(screen.getByText("Email já registrado")).toBeInTheDocument();
+  });
+
+  it("should redirect to home page if user is already logged in", () => {
+    localStorage.setItem("isLoggedIn", true);
+
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
+
+    expect(localStorage.getItem("isLoggedIn")).toBeTruthy();
+    expect(window.location.pathname).toBe("/");
+  });
 
   it("should redirect to login page when login button is clicked", async () => {
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
+
     const loginButton = screen.getByRole("button", {
       name: /entrar/i,
     });
