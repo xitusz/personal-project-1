dd60d28418bc331f872cad9f6c357f37bb5e4613
G-Alves1 <2kgabrielalves@gmail.com>
2023-05-08 12:29:28 -0300

    test(pages): Character

---
+++ front-end/src/tests/pages/character.test.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-undef */
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import Character from "../../pages/Character";
+
+describe("Character page", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <Character />
+      </BrowserRouter>
+    );
+  });
+
+  it("should renders the title correctly", async () => {
+    await waitFor(() => {
+      const title = screen.getByRole("heading", { name: /personagens/i });
+
+      expect(title).toBeInTheDocument();
+    });
+  });
+
+  it("should renders the champions correctly", async () => {
+    await waitFor(() => {
+      const mock = {
+        Aatrox: { id: "Aatrox" },
+        Ahri: { id: "Ahri" },
+      };
+
+      jest.spyOn(global, "fetch").mockImplementation(() =>
+        Promise.resolve({
+          json: () => Promise.resolve({ data: mock }),
+        })
+      );
+
+      expect(screen.getByText(/aatrox/i)).toBeInTheDocument();
+      expect(screen.getByText(/ahri/i)).toBeInTheDocument();
+
+      global.fetch.mockRestore();
+    });
+  });
+
+  it("should renders the loading message correctly", () => {
+    const mock = {};
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ data: mock }),
+      })
+    );
+
+    expect(screen.getByText(/carregando/i)).toBeInTheDocument();
+
+    global.fetch.mockRestore();
+  });
+});
