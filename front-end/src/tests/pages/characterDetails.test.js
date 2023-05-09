ab2150857154f5378010183f2fc296ed30d094e2
G-Alves1 <2kgabrielalves@gmail.com>
2023-05-09 06:40:41 -0300

    test(pages): CharacterDetails

---
+++ front-end/src/tests/pages/characterDetails.test.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-undef */
+import React from "react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import CharacterDetails from "../../pages/CharacterDetails";
+
+describe("CharacterDetails page", () => {
+  beforeEach(() => {
+    const championName = "Aatrox";
+
+    render(
+      <MemoryRouter initialEntries={[`/character/${championName}`]}>
+        <Routes>
+          <Route
+            path="/character/:championName"
+            element={<CharacterDetails />}
+          />
+        </Routes>
+      </MemoryRouter>
+    );
+  });
+
+  it("should renders the loading message correctly", () => {
+    const mock = {};
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ data: mock }),
+      })
+    );
+
+    expect(screen.getByText(/carregando/i)).toBeInTheDocument();
+
+    global.fetch.mockRestore();
+  });
+
+  it("should renders the champion details correctly", async () => {
+    await waitFor(() => {
+      expect(
+        screen.getByRole("heading", { name: /aatrox/i })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole("heading", { name: /a espada darkin/i })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole("heading", { name: /função/i })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole("heading", { name: /habilidades/i })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByRole("heading", { name: /skins/i })
+      ).toBeInTheDocument();
+    });
+  });
+});
