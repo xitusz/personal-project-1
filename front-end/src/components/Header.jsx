9f4b048b090936622b479e5a497c978e1730b807
G-Alves1 <2kgabrielalves@gmail.com>
2023-05-11 19:28:26 -0300

    style(css): position fixed and margin in Header

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -16,85 +16,93 @@ const Header = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-md navbar-light bg-light">
-      <div className="container">
-        <div className="m-auto">
-          <button
-            className="navbar-toggler"
-            type="button"
-            onClick={() => setCollapsed(!collapsed)}
+    <div style={{ marginTop: "4rem" }}>
+      <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top">
+        <div className="container">
+          <div className="m-auto">
+            <button
+              className="navbar-toggler"
+              type="button"
+              onClick={() => setCollapsed(!collapsed)}
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+          </div>
+          <div
+            className={`collapse navbar-collapse ${collapsed ? "" : "show"}`}
           >
-            <span className="navbar-toggler-icon" />
-          </button>
-        </div>
-        <div className={`collapse navbar-collapse ${collapsed ? "" : "show"}`}>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink className="nav-link" activeclassname="active" to="/">
-                Início
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                className="nav-link"
-                activeclassname="active"
-                to="/character"
-              >
-                Personagens
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                className="nav-link"
-                activeclassname="active"
-                to="/region"
-              >
-                Regiões
-              </NavLink>
-            </li>
-          </ul>
-          {getItemFromLocalStorage("user") ? (
-            <ul className="navbar-nav">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink
-                  className="nav-link"
-                  activeclassname="active"
-                  to="/profile"
-                >
-                  Perfil
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <Link to="/login" onClick={handleExiting} className="nav-link">
-                  Sair
-                </Link>
-              </li>
-            </ul>
-          ) : (
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <NavLink
-                  className="nav-link"
-                  activeclassname="active"
-                  to="/login"
-                >
-                  Login
+                <NavLink className="nav-link" activeclassname="active" to="/">
+                  Início
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink
                   className="nav-link"
                   activeclassname="active"
-                  to="/register"
+                  to="/character"
                 >
-                  Cadastro
+                  Personagens
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  activeclassname="active"
+                  to="/region"
+                >
+                  Regiões
                 </NavLink>
               </li>
             </ul>
-          )}
+            {getItemFromLocalStorage("user") ? (
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink
+                    className="nav-link"
+                    activeclassname="active"
+                    to="/profile"
+                  >
+                    Perfil
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    to="/login"
+                    onClick={handleExiting}
+                    className="nav-link"
+                  >
+                    Sair
+                  </Link>
+                </li>
+              </ul>
+            ) : (
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink
+                    className="nav-link"
+                    activeclassname="active"
+                    to="/login"
+                  >
+                    Login
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    className="nav-link"
+                    activeclassname="active"
+                    to="/register"
+                  >
+                    Cadastro
+                  </NavLink>
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
