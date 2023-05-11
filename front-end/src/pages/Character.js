8c136191e7cd2f402f34640d9c254c63ba1b4f2c
G-Alves1 <2kgabrielalves@gmail.com>
2023-05-11 20:47:01 -0300

    feat(pages): component Footer in Character

--- front-end/src/pages/Character.js
+++ front-end/src/pages/Character.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Card from "../components/Card";
+import Footer from "../components/Footer";
 
 const Character = () => {
   const [champions, setChampions] = useState([]);
@@ -40,6 +41,7 @@ const Character = () => {
           );
         })}
       </div>
+      <Footer />
     </div>
   );
 };
