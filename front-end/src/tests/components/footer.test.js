9abcaf3c5c41837e23669c7478137014954c7eb5
G-Alves1 <2kgabrielalves@gmail.com>
2023-05-16 03:16:10 -0300

    test(components): change the images to icons and replace the portfolio with the github profile

--- front-end/src/tests/components/footer.test.js
+++ front-end/src/tests/components/footer.test.js
@@ -13,23 +13,6 @@ describe("Footer component", () => {
     );
   });
 
-  it("should renders the GitHub image", () => {
-    const image = screen.getByAltText(/github/i);
-
-    expect(image).toBeInTheDocument();
-    expect(image).toHaveAttribute(
-      "src",
-      "https://user-images.githubusercontent.com/25181517/192108374-8da61ba1-99ec-41d7-80b8-fb2f7c0a4948.png"
-    );
-  });
-
-  it("should renders the GitHub link", () => {
-    const link = screen.getByRole("link", { name: /github/i });
-
-    expect(link).toBeInTheDocument();
-    expect(link).toHaveAttribute("href", "https://github.com/xitusz");
-  });
-
   it('should renders the "Voltar ao topo" image', () => {
     const image = screen.getByAltText(/voltar ao topo/i);
 
@@ -56,18 +39,14 @@ describe("Footer component", () => {
     expect(window.scrollTo).toHaveBeenCalledWith(0, 0);
   });
 
-  it("should renders the LinkedIn image", () => {
-    const image = screen.getByAltText("linkedIn");
+  it("should renders the LinkedIn icon", () => {
+    const icon = screen.getByTestId("linkedin-icon");
 
-    expect(image).toBeInTheDocument();
-    expect(image).toHaveAttribute(
-      "src",
-      "https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"
-    );
+    expect(icon).toBeInTheDocument();
   });
 
   it("should renders the LinkedIn link", () => {
-    const link = screen.getByRole("link", { name: /LinkedIn/i });
+    const link = screen.getByTestId("linkedin-link");
 
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute(
@@ -76,41 +55,30 @@ describe("Footer component", () => {
     );
   });
 
-  it("should renders the Gmail image", () => {
-    const image = screen.getByAltText("gmail");
+  it("should renders the Gmail icon", () => {
+    const icon = screen.getByTestId("gmail-icon");
 
-    expect(image).toBeInTheDocument();
-    expect(image).toHaveAttribute(
-      "src",
-      "https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white"
-    );
+    expect(icon).toBeInTheDocument();
   });
 
   it("should renders the Gmail link", () => {
-    const link = screen.getByRole("link", { name: /Gmail/i });
+    const link = screen.getByTestId("gmail-link");
 
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute("href", "mailto:2kgabrielalves@gmail.com");
   });
 
-  it("should renders the portfolio image", () => {
-    const image = screen.getByAltText("portfolio");
+  it("should renders the Github icon", () => {
+    const icon = screen.getByTestId("github-icon");
 
-    expect(image).toBeInTheDocument();
-    expect(image).toHaveAttribute(
-      "src",
-      "https://camo.githubusercontent.com/33bc5b729c9fb7f279528ca182bb84d42b9b6fb40233e494afa02fad1599bcb7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f706f7274666f6c696f2d3030303030303f7374796c653d666f722d7468652d6261646765266c6f676f3d41626f75742e6d65266c6f676f436f6c6f723d7768697465"
-    );
+    expect(icon).toBeInTheDocument();
   });
 
-  it("should renders the Portfolio link", () => {
-    const link = screen.getByRole("link", { name: /Portfolio/i });
+  it("should renders the Github link", () => {
+    const link = screen.getByTestId("github-link");
 
     expect(link).toBeInTheDocument();
-    expect(link).toHaveAttribute(
-      "href",
-      "https://xitusz.github.io/my-portfolio/"
-    );
+    expect(link).toHaveAttribute("href", "https://github.com/xitusz");
   });
 
   it("should renders the copyright message", () => {
