a892cee1a369b7fb0f8bfc9e51324b6e6c6d666a
G-Alves1 <2kgabrielalves@gmail.com>
2023-05-16 04:59:35 -0300

    style(css): Footer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Routes from "./routes";
 import "./css/Header.css";
+import "./css/Footer.css";
 
 const App = () => {
   return <Routes />;
--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -5,17 +5,16 @@ import { BsArrowUp } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div className="mt-3 bg-light">
+    <div id="footer">
       <div className="mx-5">
         <div className="w-full mx-auto p-4">
-          <div className="d-flex align-items-center justify-content-between">
-            <div className="text-end">
-              <BsArrowUp
-                size={45}
-                data-testid="arrow-icon"
-                onClick={() => window.scrollTo(0, 0)}
-              />
-            </div>
+          <div className="arrow-div">
+            <BsArrowUp
+              size={45}
+              className="text-white arrow-icon"
+              data-testid="arrow-icon"
+              onClick={() => window.scrollTo(0, 0)}
+            />
           </div>
           <div>
             <ul className="d-flex flex-wrap list-inline align-items-center justify-content-center">
@@ -24,8 +23,13 @@ const Footer = () => {
                   href="https://www.linkedin.com/in/gabrielalves1/"
                   target="_blank"
                   rel="noreferrer"
+                  data-testid="linkedin-link"
                 >
-                  <FaLinkedin size={26} data-testid="linkedin-icon" />
+                  <FaLinkedin
+                    size={26}
+                    className="text-white icon"
+                    data-testid="linkedin-icon"
+                  />
                 </a>
               </li>
               <li className="p-1">
@@ -33,8 +37,13 @@ const Footer = () => {
                   href="mailto:2kgabrielalves@gmail.com"
                   target="_blank"
                   rel="noreferrer"
+                  data-testid="gmail-link"
                 >
-                  <FiMail size={30} data-testid="gmail-icon" />
+                  <FiMail
+                    size={30}
+                    className="text-white icon"
+                    data-testid="gmail-icon"
+                  />
                 </a>
               </li>
               <li className="p-1">
@@ -42,21 +51,26 @@ const Footer = () => {
                   href="https://github.com/xitusz"
                   target="_blank"
                   rel="noreferrer"
+                  data-testid="github-link"
                 >
-                  <FaGithub size={26} data-testid="github-icon" />
+                  <FaGithub
+                    size={26}
+                    className="text-white icon"
+                    data-testid="github-icon"
+                  />
                 </a>
               </li>
             </ul>
           </div>
-          <hr className="my-4 mx-auto" />
+          <hr className="my-4 mx-auto hr-footer" />
           <div>
-            <p className="text-center">
+            <p className="text-center text-footer">
               © 2023{" "}
               <a
                 href="https://github.com/xitusz"
                 target="_blank"
                 rel="noreferrer"
-                className="text-decoration-none"
+                className="a-footer"
               >
                 Gabriel Alves
               </a>
