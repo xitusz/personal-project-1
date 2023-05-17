bb1d397c320c1e6bfd238e39765d1452f8ec169f
G-Alves1 <2kgabrielalves@gmail.com>
2023-05-17 10:29:06 -0300

    style(css): Card

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Routes from "./routes";
 import "./css/Header.css";
 import "./css/Footer.css";
+import "./css/Card.css";
 
 const App = () => {
   return <Routes />;
--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -1,42 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 const Card = ({ name, image }) => {
-  const [hovered, setHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setHovered(false);
-  };
-
   return (
-    <div
-      className="card border-0"
-      style={{
-        height: "20rem",
-        width: "15rem",
-        transform: `scale(${hovered ? 1.05 : 1})`,
-        transition: "transform 0.3s ease-out",
-      }}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
+    <div className="card border-0 card-div">
       <img
         src={image}
         alt={name}
-        className="card-img-top img-fluid h-100"
-        style={{ objectFit: "cover" }}
+        className="card-img-top img-fluid h-100 card-image"
       />
-      <div
-        className="card-body p-0"
-        style={{
-          backgroundColor: `${hovered ? "rgb(39 111 141)" : "rgb(6, 28, 37)"}`,
-          transition: "background-color 0.3s cubic-bezier(0.4, 0.0, 0.2, 1)",
-        }}
-      >
+      <div className="card-body p-0">
         <h5 className="card-title text-center pt-2 text-white">{name}</h5>
       </div>
     </div>
