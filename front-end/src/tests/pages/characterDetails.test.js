e9092fc43cff6cf80e60939b8b5ae894cec4bddb
G-Alves1 <2kgabrielalves@gmail.com>
2023-05-25 03:57:47 -0300

    test(pages): skill button and skin button in CharacterDetails

--- front-end/src/tests/pages/characterDetails.test.js
+++ front-end/src/tests/pages/characterDetails.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import React from "react";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import CharacterDetails from "../../pages/CharacterDetails";
 
 describe("CharacterDetails page", () => {
@@ -20,7 +20,7 @@ describe("CharacterDetails page", () => {
     );
   });
 
-  it("should renders the loading message correctly", () => {
+  it("should render the loading message correctly", () => {
     const mock = {};
 
     jest.spyOn(global, "fetch").mockImplementation(() =>
@@ -34,7 +34,7 @@ describe("CharacterDetails page", () => {
     global.fetch.mockRestore();
   });
 
-  it("should renders the champion details correctly", async () => {
+  it("should render the champion details correctly", async () => {
     await waitFor(() => {
       expect(
         screen.getByRole("heading", { name: /aatrox/i, level: 1 })
@@ -42,8 +42,15 @@ describe("CharacterDetails page", () => {
       expect(
         screen.getByRole("heading", { name: /a espada darkin/i })
       ).toBeInTheDocument();
+      expect(screen.getByText(/fighter/i)).toBeInTheDocument();
+      expect(screen.getByText(/tank/i)).toBeInTheDocument();
       expect(
         screen.getByRole("heading", { name: /história/i })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByText(
+          /antes defensores honrados de Shurima contra o temido vazio/i
+        )
       ).toBeInTheDocument();
       expect(
         screen.getByRole("heading", { name: /habilidades/i })
@@ -51,6 +58,44 @@ describe("CharacterDetails page", () => {
       expect(
         screen.getByRole("heading", { name: /skins/i })
       ).toBeInTheDocument();
+    });
+  });
+
+  it("should toggle skill on button click", async () => {
+    await waitFor(() => {
+      const passiveDescription = screen.getByText(
+        /periodicamente, o próximo ataque básico de aatrox/i
+      );
+
+      const skillButton = screen.getByTestId("Q-skill");
+
+      expect(passiveDescription).toBeInTheDocument();
+
+      fireEvent.click(skillButton);
+
+      expect(passiveDescription).not.toBeInTheDocument();
+
+      expect(
+        screen.getByText(
+          /Aatrox bate sua espada no chão, causando Dano Físico/i
+        )
+      ).toBeInTheDocument();
+    });
+  });
+
+  it("should toggle skin on button click", async () => {
+    await waitFor(() => {
+      const skinDefault = screen.getByTestId("default-skin");
+
+      const skin = screen.getByTestId("Aatrox Justiceiro-button");
+
+      expect(skinDefault).toBeInTheDocument();
+
+      fireEvent.click(skin);
+
+      expect(skinDefault).not.toBeInTheDocument();
+
+      expect(screen.getByTestId("Aatrox Justiceiro-skin")).toBeInTheDocument();
     });
   });
 });
