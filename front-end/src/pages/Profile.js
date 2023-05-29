80bebb777728859603bffef847f07344231ba406
G-Alves1 <2kgabrielalves@gmail.com>
2023-05-29 00:35:59 -0300

    feat(pages): text "Em construção" and link to character pages in Profile

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Header from "../components/Header";
+import Loading from "../components/Loading";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   return (
     <div>
       <Header />
-      <h1>Perfil</h1>
+      <div className="py-5">
+        <h1 className="text-center text-white pt-5 p-4">Perfil</h1>
+        <h2 className="text-center text-white pt-5 p-4">
+          Em contrução <Loading />
+        </h2>
+        <h4 className="text-center text-white pt-5 p-4">
+          Vá para{" "}
+          <Link
+            to="/character"
+            className="text-white construction-character-link"
+          >
+            Personagens
+          </Link>
+        </h4>
+      </div>
     </div>
   );
 };
