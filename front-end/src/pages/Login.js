827db001f82827892625a5595fceda8730f1d4a7
G-Alves1 <2kgabrielalves@gmail.com>
2023-05-30 16:20:52 -0300

    style(css): responsiveness for different screen sizes in Login

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -52,55 +52,57 @@ const Login = () => {
       <Header />
       <form className="container py-5">
         <h1 className="text-center text-white pt-5 p-4">Conectar-se</h1>
-        <div className="">
-          <div className="p-5 rounded-3 mb-1 form-field">
-            <div className="input-group mb-3 input-div rounded-1">
-              <span className="input-group-text form-input border-0 text-white p-2 px-3">
-                <AiOutlineMail size={23} />
-              </span>
-              <div className="form-floating">
-                <input
-                  type="email"
-                  className="form-control form-input text-white border-0 p-0"
-                  id="email"
-                  name="email"
-                  placeholder="email@example.com"
-                  value={email}
-                  onChange={handleEmailChange}
-                  required
-                />
+        <div className="row justify-content-center">
+          <div className="col-sm-12 col-md-10 col-lg-8">
+            <div className="p-5 rounded-3 mb-1 form-field">
+              <div className="input-group mb-3 input-div rounded-1">
+                <span className="input-group-text form-input border-0 text-white p-2 px-3">
+                  <AiOutlineMail size={23} />
+                </span>
+                <div className="form-floating">
+                  <input
+                    type="email"
+                    className="form-control form-input text-white border-0 p-0"
+                    id="email"
+                    name="email"
+                    placeholder="email@example.com"
+                    value={email}
+                    onChange={handleEmailChange}
+                    required
+                  />
+                </div>
               </div>
-            </div>
-            <div className="input-group mb-3 input-div rounded-1">
-              <span className="input-group-text form-input border-0 text-white p-2 px-3">
-                <BsFillKeyFill size={23} />
-              </span>
-              <div className="form-floating">
-                <input
-                  type="password"
-                  className="form-control form-input text-white border-0 p-0"
-                  id="password"
-                  name="password"
-                  placeholder="*********"
-                  value={password}
-                  onChange={handlePasswordChange}
-                  required
-                />
+              <div className="input-group mb-3 input-div rounded-1">
+                <span className="input-group-text form-input border-0 text-white p-2 px-3">
+                  <BsFillKeyFill size={23} />
+                </span>
+                <div className="form-floating">
+                  <input
+                    type="password"
+                    className="form-control form-input text-white border-0 p-0"
+                    id="password"
+                    name="password"
+                    placeholder="*********"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    required
+                  />
+                </div>
               </div>
+              <Button
+                className="btn btn-primary w-100 mb-2"
+                onClick={handleLogin}
+              >
+                Entrar
+              </Button>
+              <Link to="/register">
+                <Button className="btn btn-secondary w-100">Cadastre-se</Button>
+              </Link>
             </div>
-            <Button
-              className="btn btn-primary w-100 mb-2"
-              onClick={handleLogin}
-            >
-              Entrar
-            </Button>
-            <Link to="/register">
-              <Button className="btn btn-secondary w-100">Cadastre-se</Button>
-            </Link>
+            {error && (
+              <div className="my-3 alert alert-danger text-center">{error}</div>
+            )}
           </div>
-          {error && (
-            <div className="my-3 alert alert-danger text-center">{error}</div>
-          )}
         </div>
       </form>
     </div>
