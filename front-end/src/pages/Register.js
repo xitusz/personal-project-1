59a0b84d5db9e4a5c1ef65825b427c6214a8ea8d
G-Alves1 <2kgabrielalves@gmail.com>
2023-05-30 16:21:01 -0300

    style(css): responsiveness for different screen sizes in Register

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -68,80 +68,84 @@ const Register = () => {
       <Header />
       <form className="container py-5">
         <h1 className="text-center text-white pt-5 p-4">Cadastre-se</h1>
-        <div className="">
-          <div className="p-5 rounded-3 mb-1 form-field">
-            <div className="input-group mb-2 input-div rounded-1">
-              <span className="input-group-text form-input border-0 text-white p-2 px-3">
-                <AiOutlineUser size={23} />
-              </span>
-              <div className="form-floating">
-                <input
-                  type="text"
-                  className="form-control form-input text-white border-0 p-0"
-                  id="name"
-                  name="name"
-                  placeholder="Nome"
-                  value={name}
-                  onChange={handlenameChange}
-                  required
-                />
+        <div className="row justify-content-center">
+          <div className="col-sm-12 col-md-10 col-lg-8">
+            <div className="p-5 rounded-3 mb-1 form-field">
+              <div className="input-group mb-2 input-div rounded-1">
+                <span className="input-group-text form-input border-0 text-white p-2 px-3">
+                  <AiOutlineUser size={23} />
+                </span>
+                <div className="form-floating">
+                  <input
+                    type="text"
+                    className="form-control form-input text-white border-0 p-0"
+                    id="name"
+                    name="name"
+                    placeholder="Nome"
+                    value={name}
+                    onChange={handlenameChange}
+                    required
+                  />
+                </div>
               </div>
-            </div>
-            <div className="form-text mb-3">
-              Seu nome deve ter no mínimo 2 caracteres.
-            </div>
-            <div className="input-group mb-2 input-div rounded-1">
-              <span className="input-group-text form-input border-0 text-white p-2 px-3">
-                <AiOutlineMail size={23} />
-              </span>
-              <div className="form-floating">
-                <input
-                  type="email"
-                  className="form-control form-input text-white border-0 p-0"
-                  id="email"
-                  name="email"
-                  placeholder="email@example.com"
-                  value={email}
-                  onChange={handleEmailChange}
-                  required
-                />
+              <div className="form-text mb-3">
+                Seu nome deve ter no mínimo 2 caracteres.
               </div>
-            </div>
-            <div className="form-text mb-3">
-              Seu email deve ser um email válido.
-            </div>
-            <div className="input-group mb-2 input-div rounded-1">
-              <span className="input-group-text form-input border-0 text-white p-2 px-3">
-                <BsFillKeyFill size={23} />
-              </span>
-              <div className="form-floating">
-                <input
-                  type="password"
-                  className="form-control form-input text-white border-0 p-0"
-                  id="password"
-                  name="password"
-                  placeholder="*********"
-                  value={password}
-                  onChange={handlePasswordChange}
-                  required
-                />
+              <div className="input-group mb-2 input-div rounded-1">
+                <span className="input-group-text form-input border-0 text-white p-2 px-3">
+                  <AiOutlineMail size={23} />
+                </span>
+                <div className="form-floating">
+                  <input
+                    type="email"
+                    className="form-control form-input text-white border-0 p-0"
+                    id="email"
+                    name="email"
+                    placeholder="email@example.com"
+                    value={email}
+                    onChange={handleEmailChange}
+                    required
+                  />
+                </div>
               </div>
+              <div className="form-text mb-3">
+                Seu email deve ser um email válido.
+              </div>
+              <div className="input-group mb-2 input-div rounded-1">
+                <span className="input-group-text form-input border-0 text-white p-2 px-3">
+                  <BsFillKeyFill size={23} />
+                </span>
+                <div className="form-floating">
+                  <input
+                    type="password"
+                    className="form-control form-input text-white border-0 p-0"
+                    id="password"
+                    name="password"
+                    placeholder="*********"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="form-text mb-3">
+                Sua senha deve ter de 6 a 12 caracteres.
+              </div>
+              <Button
+                className="btn btn-primary w-100 mb-2"
+                onClick={handleRegister}
+              >
+                Cadastrar
+              </Button>
+              <Link to="/login">
+                <Button className="btn btn-secondary w-100">Entrar</Button>
+              </Link>
+              {error && (
+                <div className="my-3 alert alert-danger text-center">
+                  {error}
+                </div>
+              )}
             </div>
-            <div className="form-text mb-3">
-              Sua senha deve ter de 6 a 12 caracteres.
-            </div>
-            <Button
-              className="btn btn-primary w-100 mb-2"
-              onClick={handleRegister}
-            >
-              Cadastrar
-            </Button>
-            <Link to="/login">
-              <Button className="btn btn-secondary w-100">Entrar</Button>
-            </Link>
-            {error && (
-              <div className="my-3 alert alert-danger text-center">{error}</div>
-            )}
           </div>
         </div>
       </form>
