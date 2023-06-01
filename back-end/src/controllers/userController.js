84af045862ba96e29143c77481f0acf2da78b4ac
G-Alves1 <2kgabrielalves@gmail.com>
2023-06-01 10:57:46 -0300

    feat(controllers): login user

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -12,6 +12,19 @@ const create = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const data = await userService.login(email, password);
+
+    return res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   create,
+  login,
 };
